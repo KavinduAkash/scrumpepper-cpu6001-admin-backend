aa3859162cfe5bdceaaff3663b609a6535128831
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-02-16 04:46:18 +0530

    init starter

---
+++ src/main/java/com/swlc/ScrumPepperAdminCPU6001/ScrumPepperAdminCpu6001Application.java
@@ -0,0 +1,13 @@
+package com.swlc.ScrumPepperAdminCPU6001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumPepperAdminCpu6001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumPepperAdminCpu6001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/swlc/ScrumPepperAdminCPU6001/ScrumPepperAdminCpu6001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swlc.ScrumPepperAdminCPU6001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumPepperAdminCpu6001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
