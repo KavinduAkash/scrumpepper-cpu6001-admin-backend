8f2fe3da623d8a344c5297c937ce6805d37c209d
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-04-02 17:13:01 +0530

    fix bug at application config

--- src/main/java/com/swlc/ScrumPepperAdminCPU6001/ScrumPepperAdminCpu6001Application.java
+++ src/main/java/com/swlc/ScrumPepperAdminCPU6001/ScrumPepperAdminCpu6001Application.java
@@ -2,9 +2,16 @@ package com.swlc.ScrumPepperAdminCPU6001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ScrumPepperAdminCpu6001Application {
+public class ScrumPepperAdminCpu6001Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ScrumPepperAdminCpu6001Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrumPepperAdminCpu6001Application.class, args);
